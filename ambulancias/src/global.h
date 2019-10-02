a381c65a6a20879f2a723fe8e6815cb184175a54
alumno <alumno@LAB00PC00>
2019-10-02 19:36:51 -0300

    2 ejercs

---
+++ ambulancias/src/global.h
@@ -0,0 +1,16 @@
+#ifndef GLOBAL_H_
+#define GLOBAL_H_
+
+/* INCLUDES */
+#include "arrayEmployee.h"
+#include "stuff.h"
+#include "utn.h"
+
+/* DEFINES */
+#define EXIT_SUCCESS 0
+#define DB_LENGHT 1000
+#define MSG "\n\t\t\t\t\tPor favor, ingrese una opcion:"
+#define MSG_ERROR "\n\t\t\t\t\tERROR, ingrese una opcion valida..\n"
+#define NO_EMPLOYEES_ERROR "\n\t\t\t\t\tNo existen empleados en la base de datos"
+#define MSG_REDIRECT "\n\t\t\t\t\tRedireccionando al menu principal.."
+#endif /* GLOBAL_H_ */
---
+++ pantallas/Debug/src/arrayEmployee.d
@@ -0,0 +1,8 @@
+src/arrayEmployee.o: ../src/arrayEmployee.c ../src/arrayEmployee.h \
+ ../src/utn.h ../src/stuff.h
+
+../src/arrayEmployee.h:
+
+../src/utn.h:
+
+../src/stuff.h:
